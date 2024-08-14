362955b63296bb5e9fe2626f952b4b88cd7328c4
Facundo Tuñas <154563924+Uruwhy1@users.noreply.github.com>
2024-08-14 13:21:50 -0300

    reorganize project file structure

--- public/components/cards/cards.js
+++ public/components/cards/cards.js
@@ -1,14 +1,13 @@
 const ul = document.querySelector("ul");
 
-document.getElementById("searchBooks").addEventListener("input", function () {
+document.getElementById("searchItems").addEventListener("input", function () {
   const searchQuery = this.value.toLowerCase();
   const books = document.querySelectorAll("ul li");
 
   books.forEach((book) => {
-    const title = book.querySelector("strong").textContent.toLowerCase();
-    const description = book.querySelector("p").textContent.toLowerCase();
+    const title = book.querySelector(".title").textContent.toLowerCase();
 
-    if (title.includes(searchQuery) || description.includes(searchQuery)) {
+    if (title.includes(searchQuery)) {
       book.style.display = "";
     } else {
       book.style.display = "none";
